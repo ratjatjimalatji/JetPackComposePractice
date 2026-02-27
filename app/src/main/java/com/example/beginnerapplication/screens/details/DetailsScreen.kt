30d3674b974ac3228e5b2aff6afcab79816341f3
geeksWorkStation <ratjatjimalatji@gmail.com>
2026-02-27 10:19:25 +0200

    Added back button for navigation, onClick navController.popBackStack(

--- app/src/main/java/com/example/beginnerapplication/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/beginnerapplication/screens/details/DetailsScreen.kt
@@ -3,12 +3,16 @@ package com.example.beginnerapplication.screens.details
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -16,6 +20,14 @@ fun DetailsScreen(navController: NavController, movieData: String?){
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
             Text(text = movieData.toString())
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+            Button(onClick = {
+                navController.popBackStack() //Screens are added to the stack , this removes the current screen from the stack
+            }) {
+                Text(text = "Go back")
+            }
         }
     }
     }
